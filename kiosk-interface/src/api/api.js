701c3e6fefbb49e482c99f5ba0fba35973a09a8f
Eloissa <127953033+mselletoe@users.noreply.github.com>
2025-11-21 21:39:54 +0800

    Raspi Updates

--- kiosk-interface/src/api/api.js
+++ kiosk-interface/src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 // Change this to match your backend URL
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // FastAPI backend
+  baseURL: 'http://172.20.10.5:8000', // FastAPI backend
   timeout: 10000,
 })
 
